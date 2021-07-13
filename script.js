8ee7f8c191766fc0326ccf4b36e6741cb781ed81
lucas <lucasemanoelfq2005@gmail.com>
2021-07-12 22:52:08 -0300

    cria função de fim de jogo quando a cobrinha colide com ela mesma

--- script.js
+++ script.js
@@ -24,6 +24,7 @@ function criarCobrinha(){
     }
 }
 
+
 function drawFood(){
     context.fillStyle = 'red'
     context.fillRect(food.x, food.y, box, box)
@@ -43,19 +44,33 @@ function iniciarjogo(){
     if(snake[0].x < 0 && direction =='left') snake[0].x = 16 * box
     if(snake[0].y > 15 * box && direction == 'down') snake[0].y = 0
     if(snake[0].y < 0 && direction == 'up') snake[0].y = 16 * box
+
+    for (i = 1; i < snake.length; i ++){
+        if (snake[0].x == snake[i].x && snake[0].y == snake[i].y){
+            clearInterval(jogo)
+            alert('FIM DE JOGO')
+        }
+    }
+
     criarBG()
     criarCobrinha()
     drawFood()
-
+    
     let snakeX = snake[0].x
     let snakeY = snake[0].y
-
+    
     if (direction == 'right') snakeX += box
     if (direction == 'left') snakeX -= box
     if (direction == 'up') snakeY -= box
     if (direction == 'down') snakeY += box
-
-    snake.pop()
+    
+    if(snakeX != food.x || snakeY != food.y){
+        snake.pop()
+    }
+    else{
+        food.x = Math.floor(Math.random() * 15 + 1) * box
+        food.y = Math.floor(Math.random() * 15 + 1) * box
+    }
     
     let newHead = {
         x : snakeX,
@@ -65,4 +80,4 @@ function iniciarjogo(){
     snake.unshift(newHead)
 }
 
-let jogo = setInterval(iniciarjogo,100)
+let jogo = setInterval(iniciarjogo,120)
